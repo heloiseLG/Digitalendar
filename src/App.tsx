a24a44da05d9434703dc1e1b40dabb71b4df7fe2
heloise <hlegeldron@gmail.com>
2021-04-16 14:14:06 +0200

    e

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { CalendarComponent } from '@syncfusion/ej2-react-calendars';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <CalendarComponent></CalendarComponent>
+    </div>
+  );
+}
+
+export default App;
